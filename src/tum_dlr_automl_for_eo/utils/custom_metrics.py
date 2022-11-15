b75d22f9e376912ce7aa85f19276df0ee3a2eacc
Emre Demir <emredemireng@gmail.com>
2022-11-15 15:48:08 +0100

    added ruggedness and plotting

--- src/tum_dlr_automl_for_eo/utils/custom_metrics.py
+++ src/tum_dlr_automl_for_eo/utils/custom_metrics.py
@@ -1,4 +1,19 @@
+import os
+from datetime import datetime
+
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf
+
+
+def mean_accuracy(acc_list):
+    return float(np.mean(acc_list))
+
+
+def variance(acc_list):
+    return float(np.var(acc_list, ddof=1))
 
 
 def positive_persistence(acc_list):
@@ -21,13 +36,18 @@ def positive_persistence(acc_list):
         # check the consecutive two epochs, find common models
         current_common_models = current_top25 == previous_top25
         # compare the current common models with previous commom models
-        current_common_models = (previous_common_models == True) & (current_common_models == True) & (
-                previous_common_models == current_common_models)
+        current_common_models = (
+            (previous_common_models == True)
+            & (current_common_models == True)
+            & (previous_common_models == current_common_models)
+        )
 
         previous_common_models = current_common_models
         previous_top25 = current_top25
 
-    return len(np.where(current_common_models == True)[0]) / len(np.where(initial_common_models == True)[0])
+    return len(np.where(current_common_models == True)[0]) / len(
+        np.where(initial_common_models == True)[0]
+    )
 
 
 def negative_persistence(acc_list):
@@ -47,18 +67,77 @@ def negative_persistence(acc_list):
     for epoch in range(2, number_of_epochs):
         current_top25 = np.argsort(acc_list[:, epoch])[:top25_len]
         current_common_models = current_top25 == previous_top25
-        current_common_models = (previous_common_models == True) & (current_common_models == True) & (
-                previous_common_models == current_common_models)
+        current_common_models = (
+            (previous_common_models == True)
+            & (current_common_models == True)
+            & (previous_common_models == current_common_models)
+        )
 
         previous_common_models = current_common_models
         previous_top25 = current_top25
 
-    return len(np.where(current_common_models == True)[0]) / len(np.where(initial_common_models == True)[0])
+    return len(np.where(current_common_models == True)[0]) / len(
+        np.where(initial_common_models == True)[0]
+    )
 
-def mean_accuracy(acc_list):
-    return float(np.mean(acc_list))
+
+def ruggedness(data, lags=1):
+    """
+    Calculates ruggedness metric and returns the result.
+    By definition calculates the Autocorrelation and takes the inverse of it.
+
+    Args:
+        data (np.array): Data to calculate ruggedness.
+        lags (int): Number of lags to calculate.
+
+    Returns:
+        np.array
+    """
+    acorr = sm.tsa.acf(data, nlags=lags)
+    rugs = 1 / acorr
+
+    return rugs
 
 
-def variance(acc_list):
-    return float(np.var(acc_list, ddof=1))
+def plot_ruggedness(data, exp_name, lags=1):
+    """
+    Plots the ruggedness.
 
+    Args:
+        data (np.array): Data to calculate autocorrelation.
+        exp_name (string): Experiment name.
+        lags (int): Number of lags to calculate.
+    """
+    rugs = ruggedness(data=data, lags=lags)
+    exp_name += "_ruggedness"
+    fig_name = get_fig_name(exp_name=exp_name, lags=lags)
+    path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../../../reports/figures", fig_name)
+    )
+    plt.plot(rugs)
+    plt.xticks(range(0, lags))
+    plt.savefig(path)
+
+    pass
+
+
+def get_fig_name(exp_name, **kwargs):
+    """
+    Creates a unique figure name based on exp_name, parameters and datetime.
+
+    Args:
+        exp_name (string): Experiment name.
+
+    Returns:
+        string: Figure name.
+    """
+
+    fig_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    fig_name = f"{exp_name}_{fig_date}"
+    if kwargs:
+        for param, value in kwargs.items():
+            fig_name += "_" + str(param) + "_" + str(value)
+
+    fig_name += ".png"
+
+    return fig_name
