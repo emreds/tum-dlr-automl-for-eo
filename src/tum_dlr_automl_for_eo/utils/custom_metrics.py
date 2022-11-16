Fixed bug with persistence calculation, updated corresponding tests

src/tum_dlr_automl_for_eo/utils/custom_metrics.py
@@ -22,18 +22,19 @@ def positive_persistence(acc_list):
     top25_len = int(np.ceil(top25_len))
     previous_top25 = np.argsort(acc_list[:, 0])[-top25_len:]
 
-    previous_common_models = [True for i in range(len(previous_top25))]
+    previous_common_models = np.array([True for i in range(len(previous_top25))])
 
     for epoch in range(1, number_of_epochs):
         current_top25 = np.argsort(acc_list[:, epoch])[-top25_len:]
         # check the consecutive two epochs, find common models
         current_common_models = current_top25 == previous_top25
 
-        # compare the current common models with previous commom models
-        current_common_models = (previous_common_models == True) & (current_common_models == True) & (
-                previous_common_models == current_common_models)
 
-        positive_persistence_over_time.append(current_common_models.count(True)/float(top25_len))
+        # compare the current common models with previous commom models
+        current_common_models = (previous_common_models == True) & (current_common_models == True) \
+                                & (previous_common_models == current_common_models)
+
+        positive_persistence_over_time.append(np.count_nonzero(current_common_models)/float(top25_len))
 
         previous_common_models = current_common_models
         previous_top25 = current_top25
@@ -48,7 +49,7 @@ def negative_persistence(acc_list):
     top25_len = int(np.ceil(top25_len))
     previous_top25 = np.argsort(acc_list[:, 0])[:top25_len]
 
-    previous_common_models = [True for i in range(len(previous_top25))]
+    previous_common_models = np.array([True for i in range(len(previous_top25))])
 
     for epoch in range(1, number_of_epochs):
         current_top25 = np.argsort(acc_list[:, epoch])[:top25_len]
@@ -57,7 +58,7 @@ def negative_persistence(acc_list):
         current_common_models = (previous_common_models == True) & (current_common_models == True) & (
                 previous_common_models == current_common_models)
 
-        negative_persistence_over_time.append(current_common_models.count(True)/float(top25_len))
+        negative_persistence_over_time.append(np.count_nonzero(current_common_models)/float(top25_len))
 
         previous_common_models = current_common_models
         previous_top25 = current_top25

tests/unit/custom_metric_test.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import sys
 
 import numpy as np
-from . import helper_acc_list
+import helper_acc_list
 
 from src.tum_dlr_automl_for_eo.utils import custom_metrics
 
@@ -46,14 +46,14 @@ def test_variance():
 
 
 def test_positive_persistence():
-    assert custom_metrics.positive_persistence(helper_acc_list.random_acc_list) == 0
-    assert custom_metrics.positive_persistence(helper_acc_list.uniform_acc_list) == 1.0
+    assert custom_metrics.positive_persistence(helper_acc_list.random_acc_list) == [1.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert custom_metrics.positive_persistence(helper_acc_list.uniform_acc_list) == [1.0 for i in range(helper_acc_list.uniform_acc_list.shape[1])]
+
 
 
 def test_negative_persistence():
-    assert custom_metrics.negative_persistence(helper_acc_list.random_acc_list) == 0
-    assert custom_metrics.negative_persistence(helper_acc_list.uniform_acc_list) == 1.0
-
+    assert custom_metrics.negative_persistence(helper_acc_list.random_acc_list) == [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert custom_metrics.negative_persistence(helper_acc_list.uniform_acc_list) == [1.0 for i in range(helper_acc_list.uniform_acc_list.shape[1])]
 
 def test_ruggedness():
     results_1_lag = np.array([1, -13.99825467])