d82605d78ea725877d26a9c0cac0a0c397e5e4ee
Emre Demir <emredemireng@gmail.com>
2023-06-05 18:04:02 +0200

    updated the reqs.

--- scripts/bash_slurm/inference_metrics.sh
+++ scripts/bash_slurm/inference_metrics.sh
@@ -9,7 +9,7 @@ cp -r -n /p/project/hai_nasb_eo/data/* /dev/shm/hai_nasb_eo/
 # Run the container with support for nvidia and cuda
 module load Stages/2022  Apptainer-Tools/2022 GCCcore/.9.3.0
 
-apptainer pull docker://dockiron/mdsi2022-automl-torch1.9:latest
+# Doesn't work. Don't know why it stucks while saving the .sif file. apptainer pull docker://dockiron/mdsi2022-automl-torch1.9:latest
 # `--nv` option enables nvidia support.
 # apptainer run --nv mdsi2022-automl-torch1.9_latest.sif
 apptainer exec --nv mdsi2022-automl-torch1.9_latest.sif python3 ./scripts/inference.py
