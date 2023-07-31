b24d477458a61d45d1e62c84fdb80dcb5f22bceb
Emre Demir <emredemireng@gmail.com>
2023-07-31 13:57:36 +0200

    8 torch lightning (#28) (#29)
    * Add num_workers for DataLoader, add script for training with Pytorch Lightning
    
    * new slurm scripts and container
    
    * Set shuffle=False for validation loader
    
    * Add custom callback, default settings for Trainer
    
    * Fix the training time estimation
    
    * Fix validation_step(), add average accuracy and loss per epoch
    
    * Add DDP training
    
    * Modify args help and use built-in hparams from Lightning
    
    * changes in script
    
    * first running pipeline
    
    * adding logs to the lightning training
    
    * change early stopping mode to max validation accuracy
    
    * Pull request: generate and analyze eo-nas-bench data (#26)
    
    * scripts for: analyzing custom eo-related nasbenchmark (fitness, latency, landsape  with ruggedness), generating NASLib-conform database
    
    * fix of script and adding of folders for results
    
    * 8 torch lightning (#27)
    
    * scripts for: analyzing custom eo-related nasbenchmark (fitness, latency, landsape  with ruggedness), generating NASLib-conform database
    
    * fix of script and adding of folders for results
    
    ---------
    
    
    
    ---------
    
    Co-authored-by: Viet Nguyen <vietnguyenthe97@gmail.com>
    Co-authored-by: Emre Demir <demir2@jwlogin04.juwels>
    Co-authored-by: Emre Demir <demir2@jwlogin06.juwels>
    Co-authored-by: Emre Demir <demir2@jwlogin05.juwels>
    Co-authored-by: Emre Demir <demir2@jwlogin08.juwels>
    Co-authored-by: René TR <kalifou@users.noreply.github.com>
    Co-authored-by: kalifou <renetrao@gmail.com>

--- scripts/bash_slurm/slurm_job.sh
+++ scripts/bash_slurm/slurm_job.sh
@@ -13,6 +13,7 @@
 # After the slurm upgrade on 22.05 the following line is required to get the correct number of cpus per task.
 export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
+
 # go to the repository directory                                                                                                                                                                                   
 cd /p/project/hai_nasb_eo/emre/tum-dlr-automl-for-eo/
 
